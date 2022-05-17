52255c108f750f9dc1c5436cb5029d3bf471ee94
GustavoFeliciano <gustavo08_fonseca@outlook.com>
2022-05-17 18:21:59 -0300

    Criação do arquivo index.php para teste com o header do site

---
+++ index.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="header.css" type="text/css">
+
+<html>
+    <header>
+        <div class = "inline_itens">
+            <ul>
+                <li>
+                    <a href="index.php"><image src = "Img/Logo.png" width = "50" height = "50"></image></a>  
+                </li>
+            </ul>
+            <ul>
+                <li id = "right">
+                    <li>
+                        <a href="index.php">Início</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contato</a>
+                    </li>
+                </li>
+            </ul>    
+        </div>
+    </header>
+</html>
