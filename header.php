92b3a188ce8e84b7c50c535e00db2ed845946f9f
GustavoFeliciano <gustavo08_fonseca@outlook.com>
2022-05-18 16:02:11 -0300

    Otimizacao do header, ainda não finalizado

--- header.php
+++ header.php
@@ -1,21 +1,9 @@
-<link rel="stylesheet" href="header.css" type="text/css">
+    <link rel="stylesheet" href="header.css" type="text/css">
 
 <header>
-    <div class = "inline_itens">
-        <ul>
-            <li>
-                <a href="index.php"><image src = "Img/Logo.png" width = "50" height = "50"></image></a>  
-            </li>
-        </ul>
-        <ul>
-            <li id = "right">
-                <li>
-                    <a href="index.php">Início</a>
-                </li>
-                <li>
-                    <a href="contact.php">Contato</a>
-                </li>
-            </li>
-        </ul>    
-    </div>
+    <nav>
+        <image id="Logo" src = "Img/Logo.png" width = "100" height = "100"></image>
+        <a class="teste" href="index.php">Início</a>
+        <a class="teste" ref="contact.php">Contato</a>
+    </nav>
 </header>
