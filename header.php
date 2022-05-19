74f826baf28e90ee3605e803dc3df4bfa235b2b5
GustavoFeliciano <gustavo08_fonseca@outlook.com>
2022-05-19 17:51:43 -0300

    Pack de otimizacão 0.0.1
    Otimizar o header do site, adicionando novas div para separacão do nav

--- header.php
+++ header.php
@@ -1,9 +1,22 @@
     <link rel="stylesheet" href="header.css" type="text/css">
 
 <header>
-    <nav>
-        <image id="Logo" src = "Img/Logo.png" width = "100" height = "100"></image>
-        <a class="teste" href="index.php">Início</a>
-        <a class="teste" ref="contact.php">Contato</a>
+    <nav role="navigation">
+        <div class="header_nav_bar_div" id = "left_nav_bar_div">
+            <span>
+                <a href="index.php" target="_self">
+                    <image id="Logo" src = "Img/Logo.png" width = "100" height = "100"></image>
+                </a>
+            </span>
+            <span>
+                <a href="index.php" target="_self">
+                    Gastec Aquecedores
+                </a>
+            </span>
+        </div>
+        <div class="header_nav_bar_div" id = "right_nav_bar_div">
+            <a href="index.php" target="_self">Início</a>
+            <a ref="contact.php" target="_self">Contato</a>
+        </div>
     </nav>
 </header>
