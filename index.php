ec59572c285342d4bfff3e175316496059aa01f6
GustavoFeliciano <gustavo08_fonseca@outlook.com>
2022-05-20 16:23:36 -0300

    Pack de feature 0.0.1 home
    adicionando código no index.php e começando os primeiros estilos com o arquivo home.css

--- index.php
+++ index.php
@@ -1,9 +1,44 @@
-<link rel="stylesheet" href="index.css" type="text/css">
-<html>
-    <body>
-        <?php
-        include_once "header.php";
-        include_once "footer.php";
-        ?>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css" type="text/css">
+    <link rel="stylesheet" href="home.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include_once "header.php";
+    ?>
+    
+    <div class="intent">
+        <div class="intent_home_div" id="about_us_div_text">
+
+            //TODO
+        </div>
+        <div class="intent_home_div" id="intent_about_us_div">
+            <nav>
+                <div id="about_us_img_div">
+                    <img id="about_us_main_img" src="Img\about_us_main_img.jpeg">
+                </div>
+                <div >
+                    Quem somos nós?
+                    aa
+                </div>
+            </nav>
+        </div>
+        <div class="intent_home_div" id="services_div">
+            //TODO
+        </div>
+        <div class="intent_home_div" id="place_div">
+            //TODO
+        </div>
+    </div>
+
+    <?php
+    include_once "footer.php"; 
+    ?>
+</body>
 </html>
