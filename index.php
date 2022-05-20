c629c564c6c2e8aa33afa22596d4b86f3f0dc699
GustavoFeliciano <gustavo08_fonseca@outlook.com>
2022-05-20 19:40:24 -0300

    Pack de otimizacao 0.0.1 home
    Consertando a tela home, as divs estão distribuidas de maneira correta, preparando para próximo dia de atualizacao, 22/05/2022

--- index.php
+++ index.php
@@ -21,11 +21,17 @@
         <div class="intent_home_div" id="intent_about_us_div">
             <nav>
                 <div id="about_us_img_div">
-                    <img id="about_us_main_img" src="Img\about_us_main_img.jpeg">
+                    <span>
+                        <img id="about_us_main_img" src="Img\about_us_main_img.jpeg">
+                    </span>
                 </div>
-                <div >
-                    Quem somos nós?
-                    aa
+                <div id="about_us_text_div">
+                    <span>
+                        <p id="about_us_title_text"><h1>Sobre nós</h1></p>
+                        <p id="about_us_general_text">Somos a Gasotec, uma empresa especializada em atender o cliente,
+                        </br> 
+                        nós fazemos a sua manutenção, e vendemos a você a melhor oferta que puder encontrar</p>
+                    </span>
                 </div>
             </nav>
         </div>
